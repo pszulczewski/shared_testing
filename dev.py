2e4ea1725e346cab78498a175882083dfcdfafaf
pszulczewski <patryk@szulczewski.eu>
2024-06-12 16:40:25 +0200

    Prefix with CF

--- dev.py
+++ dev.py
@@ -13,6 +13,27 @@ payload = {
     "router_id": {"address": "104.94.130.210/29"}
 }
 
-ri = nautobot.plugins.bgp.routing_instances.create(**payload)
 
-print(ri)
+payload = {
+    "prefix": "9.9.9.9/10",
+    "status": "Active",
+    "role": "pop",
+    "cf_date_added": "1/1/2024, 0:00",  # this seems to be not working
+}
+
+ri = nautobot.ipam.prefixes.create(**payload)
+print(ri.custom_fields["date_added"])  # still None
+from pprint import pprint
+pprint(dict(ri))
+
+# Let's try to add it on the object level
+ri.custom_fields["date_added"] = payload["cf_date_added"]
+# and save the object
+ri.save()
+
+# Refresh the object with .get()
+ri = nautobot.ipam.prefixes.get(prefix="9.9.9.9/10", depth=1)
+print(ri.custom_fields["date_added"])  # Now the value is present.
+pprint(dict(ri))
+
+ri.delete()
